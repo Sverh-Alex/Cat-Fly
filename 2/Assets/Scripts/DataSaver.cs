642a236d15a20fe8d3f589acf69b8cbb780cf05c
Sverh-Alex <izialex4112@gmail.com>
2025-05-06 23:04:21 +0300

    4 commit
    всё ещё разбираюсь со скриптом магазина

--- 2/Assets/Scripts/DataSaver.cs
+++ 2/Assets/Scripts/DataSaver.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class DataSaver : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject select;
     public string objectName;
     public int id;
 
     public void SaveInt()
     {
         PlayerPrefs.SetInt("skin", id);
+        select.SetActive(false);
     }
 }
--- 2/Assets/Scripts/PlayerChanger.cs
+++ 2/Assets/Scripts/PlayerChanger.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 
 public class PlayerChanger : MonoBehaviour
 {
-    public List<GameObject> skins;
-    public Vector3 position;
-    public int skinsId;
+    public GameObject[] skins; // Ссылки на объекты скинов
+    public int skinsId = 0;
     void Start()
     {
-        skinsId = PlayerPrefs.GetInt("skin");
-        Instantiate(skins[skinsId], position, Quaternion.identity);
+        LoadSkin(); // Загрузка последнего выбранного скина при запуске
+        UpdateSkin(); // Обновление видимости скинов
+
+        // skinsId = PlayerPrefs.GetInt("skin");
+        //Instantiate(skins[skinsId], position, Quaternion.identity);
     }
-
-    // Update is called once per frame
-
+    public void LoadSkin()
+    {
+        skinsId = PlayerPrefs.GetInt("skin", 0); // Получаем сохраненный ID скина. Если нет - возвращаем 0
+    }
+    public void UpdateSkin()
+    {
+        for (int i = 0; i <= skins.Length; i++)
+        {
+            if (skins[i] !=null)
+            {
+                skins[i].SetActive(i == skinsId); // Активируем скин, если его ID соответствует выбранному
+            }
+        }
+    }
 
 }
