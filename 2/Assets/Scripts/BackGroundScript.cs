3257ae3080c7aa2bca13d029ea3f63bfa88b790b
Sverh-Alex <izialex4112@gmail.com>
2025-12-28 20:37:23 +0300

    Revert "авыф"
    This reverts commit 691a64ac50044d89537bedb53acb609c4a9cef07.

--- 2/Assets/Scripts/BackGroundScript.cs
+++ 2/Assets/Scripts/BackGroundScript.cs
@@ -2,59 +2,33 @@ using UnityEngine;
 
 public class BackGroundScript : MonoBehaviour
 {
-    [Header("Background Layers")]
-    [SerializeField] private GameObject[] backgrounds = new GameObject[3]; // Fon1, Fon2, Fon3
-    [SerializeField] private float[] speeds = { 9f, 6f, 3f }; // Скорости слоёв
-
-    [Header("Settings")]
-    [SerializeField] private Camera mainCamera;
-    [SerializeField] private float resetDistance = 40f; // Расстояние для перестановки
-
-    private Vector3[] startPositions;
-    private float[] backgroundWidths;
+    [SerializeField] GameObject Fon1;
+    [SerializeField] GameObject Fon2;
+    [SerializeField] GameObject Fon3;
+    [SerializeField] private float speed1 = 9.0f;  // Показываем на панели скорость
+    [SerializeField] private float speed2 = 6.0f;  // Показываем на панели скорость
+    [SerializeField] private float speed3 = 3.0f;  // Показываем на панели скорость
 
     void Start()
     {
-        if (mainCamera == null)
-            mainCamera = Camera.main;
 
-        // Инициализация массивов
-        startPositions = new Vector3[backgrounds.Length];
-        backgroundWidths = new float[backgrounds.Length];
 
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            if (backgrounds[i] != null)
-            {
-                startPositions[i] = backgrounds[i].transform.position;
-                backgroundWidths[i] = backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x;
-            }
-        }
     }
 
+    // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < backgrounds.Length; i++)
+        if(Fon1 != null)
         {
-            if (backgrounds[i] == null) continue;
-
-            GameObject bg = backgrounds[i];
-
-            // Двигаем фон влево
-            bg.transform.Translate(Vector3.right * -speeds[i] * Time.deltaTime);
-
-            // Проверяем, ушёл ли фон за экран
-            float distanceFromStart = bg.transform.position.x - startPositions[i].x;
-
-            if (distanceFromStart < -backgroundWidths[i] - resetDistance)
-            {
-                // ПЕРЕНОСИМ В КОНЕЦ
-                bg.transform.position = new Vector3(
-                    startPositions[i].x + backgroundWidths[i] * 2,
-                    startPositions[i].y,
-                    startPositions[i].z
-                );
-            }
+            Fon1.transform.Translate(Vector3.right * -speed1 * Time.deltaTime); // Translate - это переместить
+        }
+        if (Fon2 != null)
+        {
+            Fon2.transform.Translate(Vector3.right * -speed2 * Time.deltaTime); // Translate - это переместить
+        }
+        if (Fon3 != null)
+        {
+            Fon3.transform.Translate(Vector3.right * -speed3 * Time.deltaTime); // Translate - это переместить
         }
     }
 }
