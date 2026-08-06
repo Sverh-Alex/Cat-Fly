19d9a2d971dd7a496b739d989e8eb5300c260300
Sverh-Alex <izialex4112@gmail.com>
2026-08-06 19:00:49 +0300

    118 commit
    update save skin

--- 2/Assets/Scripts/ImageChangerMain.cs
+++ 2/Assets/Scripts/ImageChangerMain.cs
@@ -1,28 +1,54 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using PlayerPrefs = RedefineYG.PlayerPrefs;
-//using static UnityEngine.InputManagerEntry;
 
 public class ImageChangerMain : MonoBehaviour
 {
-    public Sprite[] skins; // Массив GameObject скинов
-    [SerializeField] Image image; // изображение кота на главном экране
+    public static event Action<int> OnSkinChanged;
+
+    [SerializeField] private Sprite[] skins;
+    [SerializeField] private Image image;
     private int skinId = 0;
 
     void Start()
     {
-        // Загружаем сохранённый скин, по умолчанию 0
         skinId = PlayerPrefs.GetInt("skin", 0);
-        
+        ApplySkin();
+
+        // Подписываемся на событие смены скина
+        OnSkinChanged += HandleSkinChanged;
     }
 
-    public void Update()
+    private void OnDestroy()
     {
-        skinId = PlayerPrefs.GetInt("skin");
-        if (image != null && skins != null && skinId < skins.Length)
+        OnSkinChanged -= HandleSkinChanged;
+    }
+
+    private void ApplySkin()
+    {
+        if (image != null && skins != null && skinId >= 0 && skinId < skins.Length)
         {
             image.sprite = skins[skinId];
-            Debug.Log($"установлена картинка скина{skinId}");
+            Debug.Log($"Установлена картинка скина {skinId}");
         }
+        else
+        {
+            Debug.LogWarning($"Скин {skinId} не найден или не назначен");
+        }
+    }
+
+    private void HandleSkinChanged(int newSkinId)
+    {
+        skinId = newSkinId;
+        ApplySkin();
+    }
+
+    // Вызывай этот метод при смене скина из любого другого места
+    public static void SetSkin(int newSkinId)
+    {
+        PlayerPrefs.SetInt("skin", newSkinId);
+        PlayerPrefs.Save();
+        OnSkinChanged?.Invoke(newSkinId);
     }
 }
